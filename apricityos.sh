c4f124b52b4243f6572b1621c44bedea16ac37c3
Ted Lin <fovwin@gmail.com>
2016-07-03 03:44:03 +0000

    Update apricityos.sh

--- apricityos.sh
+++ apricityos.sh
@@ -1,7 +1,18 @@
 
+## alias
+alias ll="ll -la"
+
 ## tmux
 sudo pacman -Syu
 sudo pacman -S tmux --noconfirm 
+
+## sublime
+mkdir ~/App
+sudo curl https://download.sublimetext.com/sublime_text_3_build_3114_x64.tar.bz2 -o ~/App/sublime_text_3_build_3114_x64.tar.bz2
+cd ~/App
+tar xjf sublime_text_3_build_3114_x64.tar.bz2
+sudo rm sublime_text_3_build_3114_x64.tar.bz2
+alias subl="~/App/sublime_text_3/sublime_text"
 
 ## shadowsocks for fucking GFW
 sudo pip install shadowsocks
