0b8a381c23f2b2acca907e1c0e98a47874d75c34
Ted Lin <fovwin@gmail.com>
2016-07-03 04:00:08 +0000

    Update apricityos.sh
    remove sublime because of it's too heavy

--- apricityos.sh
+++ apricityos.sh
@@ -7,12 +7,12 @@ sudo pacman -Syu
 sudo pacman -S tmux --noconfirm 
 
 ## sublime
-mkdir ~/App
-sudo curl https://download.sublimetext.com/sublime_text_3_build_3114_x64.tar.bz2 -o ~/App/sublime_text_3_build_3114_x64.tar.bz2
-cd ~/App
-tar xjf sublime_text_3_build_3114_x64.tar.bz2
-sudo rm sublime_text_3_build_3114_x64.tar.bz2
-alias subl="~/App/sublime_text_3/sublime_text"
+#mkdir ~/App
+#sudo curl https://download.sublimetext.com/sublime_text_3_build_3114_x64.tar.bz2 -o ~/App/sublime_text_3_build_3114_x64.tar.bz2
+#cd ~/App
+#tar xjf sublime_text_3_build_3114_x64.tar.bz2
+#sudo rm sublime_text_3_build_3114_x64.tar.bz2
+#alias subl="~/App/sublime_text_3/sublime_text"
 
 ## shadowsocks for fucking GFW
 sudo pip install shadowsocks
