cacab1c80a57660fcbc5ec4e168f58520ae1b6fe
Jinhui-Lin <fovwin@gmail.com>
2017-03-12 21:23:09 +0800

    Create ubuntu.sh

---
+++ ubuntu.sh
@@ -0,0 +1,33 @@
+
+## ubuntu update
+sudo apt-get update
+
+## source file list
+
+## aptitude
+sudo apt-get install aptitude -y
+
+## Git
+sudo apt-get install git -y
+sh -c "$(curl -fSL https://raw.githubusercontent.com/mintisan/oh-my-git/master/install.sh)"
+
+## tmux & tmux.conf
+sudo apt-get install tmux -y
+sh -c "$(curl -fSL https://raw.githubusercontent.com/mintisan/oh-my-tmux/master/install.sh)"
+### Prefix + I
+### source ~/.tmux.conf
+
+## ctags & cscope
+sudo apt-get install ctags cscope ack vim -y
+## vim vimrc
+sh -c "$(curl -fSL https://raw.githubusercontent.com/mintisan/oh-my-vim/master/install.sh)"
+### :PluginInstall
+### source ~/.vimrc
+
+## zsh
+sudo apt-get install autojump -y
+sh -c "$(curl -fSL https://raw.githubusercontent.com/mintisan/oh-my-zsh/master/install.sh)"
+### sudo chsh -s $(which zsh)
+### add autojump plugin
+### source ~/.zshrc
+
